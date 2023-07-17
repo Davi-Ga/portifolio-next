7f7c41846fb99bcbfd949cbf6c0f54c694f74582
Davi-Ga <davigaldinoky@gmail.com>
2023-07-17 19:12:07 -0300

    feat: navbar criada com colorMode

---
+++ src/layout/NavBar.jsx
@@ -0,0 +1,36 @@
+'use client'
+
+import {
+  Box,
+  Flex,
+  Button,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('mint', 'raisin')} px={4}>
+        <Flex h={'4rem'} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>Logo</Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <SunIcon /> : <MoonIcon /> }
+              </Button>
+
+              
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
